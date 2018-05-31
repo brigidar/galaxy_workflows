06b615c90b71e6cd1ef6e2378fd70b9b2f43d537
brigidar <brigida.a.rusconi@gmail.com>
2018-05-31 10:21:48 -0500

    removed info
    not all species have additional subtyping and doesn't work if empty.

--- tools/SNPDEF/common/plasmids/plasmid.sh
+++ tools/SNPDEF/common/plasmids/plasmid.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-esearch -db nucleotide -query "$1 [Organism]AND $2 [All Fields] AND Plasmid[filter]" | efetch --format fasta > output.fasta
+esearch -db nucleotide -query "$1 [Organism] AND Plasmid[filter]" | efetch --format fasta > output.fasta
